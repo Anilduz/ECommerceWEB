e1883eaa6cee1b7222e20c9d5597e7881ad622a0
Anilduz <anilduz34@gmail.com>
2023-01-03 14:29:26 +0300

    Update UserModel.cs

--- DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
+++ DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
@@ -2,7 +2,7 @@
 namespace DevBaykusWeb.Models
 {
 	public class UserModel
-	{ //test anıl
+	{ 
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Surname { get; set; }
@@ -12,7 +12,6 @@ namespace DevBaykusWeb.Models
         public string CreditCardInfos { get; set; }
 		public DateTime LastLogoutDate { get; set; }
 		public string IsActive { get; set; }
-		//anilduz tarafından ellendi.!!!
 		
 	}
 }
