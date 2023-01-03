7f7fa8c7e2912e5d0e3024701fc245bac195d2fd
Anilduz <anilduz34@gmail.com>
2023-01-03 14:32:50 +0300

    Update UserModel.cs

--- DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
+++ DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 namespace DevBaykusWeb.Models
 {
-	public class UserModel
-	{ //test anıl asdasd asd
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public string Surname { get; set; }
-		public string PhoneNumber { get; set; }
-		public string Address { get; set; }
+    public class UserModel
+<<<<<<< duez
+    { //test anıl asdasd asd
+=======
+<<<<<<< HEAD
+	{ 
+=======
+	{ //test anıl asdasd
+>>>>>>> 7a9216baa9d0fbeb474318bea67fe02a18edd4c0
+>>>>>>> main
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
         public string Address_2 { get; set; }
         public string CreditCardInfos { get; set; }
-		public DateTime LastLogoutDate { get; set; }
-		public string IsActive { get; set; }
+        public DateTime LastLogoutDate { get; set; }
+        public string IsActive { get; set; }
+<<<<<<< HEAD
+=======
 		//anilduz tarafından ellendi.!!! before merge, required pull rqst
-		
-	}
+>>>>>>> 7a9216baa9d0fbeb474318bea67fe02a18edd4c0
+
+    }
 }
 
