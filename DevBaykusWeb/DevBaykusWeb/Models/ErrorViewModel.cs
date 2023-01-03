e96f83fdd348e2483838792d553fae0395cc7dfc
Anilduz <anilduz34@gmail.com>
2023-01-03 10:52:26 +0300

    create DEVBAYKUSWEB CLIENT
    create DEVBAYKUSWEB CLIENT

---
+++ DevBaykusWeb/DevBaykusWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevBaykusWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
