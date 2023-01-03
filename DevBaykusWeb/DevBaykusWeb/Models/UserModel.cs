ff78e7bb3740994db90efd30ed56fdeadce078d3
Anilduz <anilduz34@gmail.com>
2023-01-03 13:57:01 +0300

    anıl düz test

--- DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
+++ DevBaykusWeb/DevBaykusWeb/Models/UserModel.cs
@@ -2,7 +2,7 @@
 namespace DevBaykusWeb.Models
 {
 	public class UserModel
-	{ //test anıl
+	{ //test anıl asdasd
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Surname { get; set; }
